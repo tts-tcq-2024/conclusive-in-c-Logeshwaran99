a9129be9092f77df00c0dc375f8cefb93d85cfb0
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-08-24 07:30:44 +0530

    Update cooling_limits.h

--- cooling_limits.h
+++ cooling_limits.h
@@ -1,4 +1,11 @@
 #pragma once
 #include "typewise-alert.h"
 
+#define LOWER_LIMIT_PASSIVE_COOLING 0
+#define UPPER_LIMIT_PASSIVE_COOLING 35
+#define LOWER_LIMIT_MED_ACTIVE_COOLING 0
+#define UPPER_LIMIT_MED_ACTIVE_COOLING 40
+#define LOWER_LIMIT_HI_ACTIVE_COOLING 0
+#define UPPER_LIMIT_HI_ACTIVE_COOLING 45
+
 void getCoolingLimits(CoolingType coolingType, int* lowerLimit, int* upperLimit);
