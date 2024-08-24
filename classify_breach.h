35115dbcb675ac2092ff5b151af5c400c5ba67b7
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-08-24 12:47:03 +0530

    Update and rename classify_breach.h to typewise-alert.h

--- classify_breach.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "typewise-alert.h"
-
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
---
+++ typewise-alert.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "cooling_type.h"
+#include "breach_type.h"
+#include "alert_target.h"
+#include "battery_character.h"
+#include "breach_classifier.h"
+#include "alert_handler.h"
+#include "alert_to_controller.h"
+#include "email_alert.h"
