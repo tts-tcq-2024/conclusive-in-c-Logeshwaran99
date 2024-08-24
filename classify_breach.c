f1d5e7994328c967006c9a1b80dec32f1ecb9697
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-08-24 07:16:20 +0530

    Create classify_breach.c

---
+++ classify_breach.c
@@ -0,0 +1,9 @@
+#include "classify_breach.h"
+#include "cooling_limits.h"
+#include "infer_breach.h"
+
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
+    int lowerLimit, upperLimit;
+    getCoolingLimits(coolingType, &lowerLimit, &upperLimit);
+    return inferBreach(temperatureInC, lowerLimit, upperLimit);
+}
