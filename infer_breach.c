d297da9b95e1ad380ca552543508a98b84fa2705
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-08-24 07:14:12 +0530

    tpewise-alert.h to infer_breach.c

---
+++ infer_breach.c
@@ -0,0 +1,11 @@
+#include "infer_breach.h"
+
+BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
+    if (value < lowerLimit) {
+        return TOO_LOW;
+    }
+    if (value > upperLimit) {
+        return TOO_HIGH;
+    }
+    return NORMAL;
+}
